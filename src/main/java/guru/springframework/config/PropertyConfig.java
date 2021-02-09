ef9b647ecda6e2473707f6fe37cf3ccfab667b2c
Developer <jjstrain68@gmail.com>
2021-02-09 16:10:04 +0000

    Refactor code to use application.properties, typical Spring Boot setup. Commented out the code for looking up the datasource.properties and jms.properties

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -14,10 +14,10 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
  */
 @Configuration
 //@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
+//@PropertySources({
+//        @PropertySource("classpath:datasource.properties"),
+//        @PropertySource("classpath:jms.properties")
+//})
 public class PropertyConfig {
 
     @Value("${guru.username}")
@@ -57,10 +57,10 @@ public class PropertyConfig {
         return jmsBroker;
     }
 
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties(){
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer =new PropertySourcesPlaceholderConfigurer();
-        return  propertySourcesPlaceholderConfigurer;
-    }
+   // @Bean
+   // public static PropertySourcesPlaceholderConfigurer properties(){
+   //     PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer =new PropertySourcesPlaceholderConfigurer();
+   //     return  propertySourcesPlaceholderConfigurer;
+   // }
 
 }
